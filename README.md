37003f7d40ec60e4dde6f0336cbcc44b787141e8
ergz <emnlrodriguez@gmail.com>
2024-01-06 09:31:42 -0800

    more work

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 the course is this one:    https://build-your-own.org/webserver/04_promise 
+
+
--- scratch.mjs
+++ scratch.mjs
@@ -1,55 +1,65 @@
 import * as net from "net";
 
-function newConn(socket) {
-	console.log("new connection", socket.remoteAddress, socket.remotePort);
-	socket.on("end", () => {
-		console.log("EOF");
-	})
-
-	socket.on("data", (data) => {
-		console.log("data", data);
-		socket.write(data);
-
-		if (data.includes("q")) {
-			console.log("closing");
-			socket.end();
+/* serveClient
+ *
+ * read data from client connection, log the data and write the data back to client
+ * */
+async function serveClient(socket) {
+	const conn = new TCPConn(socket);
+	while (true) {
+		const data = await soRead(conn);
+		if (data.length === 0) {
+			console.log("end connection");
+			break;
 		}
-	})
+		console.log("data", data);
+		await soWrite(conn, data);
+	}
 }
+
+/* newConn 
+ *
+ * serves data the client once connection has been established, checks for errors and destroys if neccesary
+ * */
+async function newConn(socket) {
+	console.log("new connection", socket.remoteAddress, socket.remotePort);
+	try {
+		await serveClient(socket);
+	} catch (exc) {
+		console.log("exception:", exc);
+	} finally {
+		socket.destroy();
+	}
+}
+
 
 let server = net.createServer({pauseOnConnect: true});
 server.on("error", (err) => { throw err; })
 server.on("connection", newConn);
-server.listen({"host": "127.0.0.1", "port": "1234"});
+	server.listen({"host": "127.0.0.1", "port": "1234"});
 
-
-class TCPConn {
+/* 
+ * TCPConn is a class to store data for the socket object. 
+ * 
+ * */
+class TCPConn { 
 	constructor(socket, reader = null, err = null, ended = false) {
 		this.socket = socket;
 		this.reader = reader;
-		this.err = null;
-		this.ended = null;
+		this.err = err;
+		this.ended = ended;
 	}
 }
 
-function soRead(conn) {
-	console.assert(!conn.reader); // assert connection is not already handling a promise
-	return new Promise((resolve, reject) => {
-		if (conn.err) {
-			reject(conn.err);
-			return;
-		}
-		if (conn.ended) {
-			resolve(Buffer.from(""));
-			return;
-		}							
-		conn.reader = {resolve: resolve, reject: reject};
-		conn.socket.resume();
-	});
-}
-function soWrite(conn, data);
+
+/* soInit 
+ *
+ * The soInit function wraps a socket object from net.socket 
+ * */
 function soInit(socket) {
-	const conn = new TCPConn(socket, reader = null, err = null, ended = false);
+	// create a tcp connection object
+	const conn = new TCPConn(socket);
+	
 	socket.on("data", (data) => {
 		console.assert(conn.reader);
 		conn.socket.pause();
@@ -58,15 +68,14 @@ function soInit(socket) {
 	})
 
 	socket.on("end", () => {
-		conn.ended = true;
+		conn.ended = true;	
 		if (conn.reader) {
 			conn.reader.resolve(Buffer.from(""));
 			conn.reader = null;
-		}
-
+		}	
 	})
 
-	socket.on("errpr", (err) => {
+	socket.on("error", (error)=> {
 		conn.err = err;
 		if (conn.reader) {
 			conn.reader.reject(err);
@@ -76,4 +85,43 @@ function soInit(socket) {
 	return conn;
 }
 
+
+function soRead(conn) {
+	console.assert(!conn.reader); // no concurrent calls
+
+	return new Promise((resolve, reject) => {
+		if (conn.err) {
+			reject(conn.err);
+			return;
+		}
+
+		if (conn.ended) {
+			resolve(Buffer.from(""));
+			return;
+		}
+		conn.reader = {resolve: resolve, reject: reject};
+		conn.resume();
+
+	})
+}
+
+
+function soWrite(conn, data) {
+	console.assert(data.length > 1);
+
+	return new Promise((resolve, reject) => {
+		if (conn.err) {
+			reject(conn.err);
+			return;
+		}
+
+		conn.socket.write(data, (err) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve();
+			}
+		})
+	})
+}
 
